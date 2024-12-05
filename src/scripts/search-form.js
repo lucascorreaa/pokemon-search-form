055b0d15e8350f31ff8211a2ec95e7d16499bdc8
lucascorreaa <lucas.lca14@gmail.com>
2024-12-04 21:32:21 -0300

    feat: new js structure

--- src/scripts/search-form.js
+++ src/scripts/search-form.js
@@ -1,36 +1,48 @@
-const form = document.getElementById('pokemon-form')
-const input = document.getElementById('pokemon-name')
-const infoContainer = document.getElementById('pokemon-info')
+const form = document.getElementById("pokemon-form");
+const input = document.getElementById("pokemon-name");
+const infoContainer = document.getElementById("pokemon-info");
 
 async function getPokemon(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    const pokemonName = input.value.trim().toLowerCase()
-    input.value = ''
-    
-    try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-        if(!response.ok) {
-            throw new Error('Pokemon not found!')
-        }
-        const pokemon = await response.json()
+  const pokemonName = input.value.trim().toLowerCase();
+  input.value = "";
 
-        infoContainer.innerHTML = `
-            <div class="card">
-            <img src="${pokemon.sprites.front_default}" alt="{pokemon.name}">
-            <h2>${pokemon.name}</h2>
-            <p><strong>Height:</strong>${pokemon.height / 10} m</p>
-            <p><strong>Weight:</strong>${pokemon.weight / 10} kg</p>
-            <p><strong>Tipos:</strong>${pokemon.types.map(type => type.type.name).join(', ')}</p>
-            </div>
-        `
-    } catch (error) {
-        console.error('Error', error)
+  try {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
 
-        infoContainer.innerHTML = `
-            <p class="error-text" >Erro: Pokemon not found. Try again in a few minutes</p>
-        `
+    if (!response.ok) {
+      throw new Error("Pokemon not found!");
     }
+
+    const pokemon = await response.json();
+
+    infoContainer.innerHTML = `
+            <div class="card">
+            <!-- .other["official-artwork"] fã art-->
+                <img class="card-img" src="${
+                  pokemon.sprites.front_default
+                }" alt="${pokemon.name}" >
+                <div class="card-aside">
+                    <h3>#${pokemon.order}</h3>
+                    <h2>${pokemon.name}</h2>
+                    <p><strong>Height:</strong>${pokemon.height / 10} m</p>
+                    <p><strong>Weight:</strong>${pokemon.weight / 10} kg</p>
+                    <p><strong>Types:</strong>${pokemon.types
+                      .map((type) => type.type.name)
+                      .join(", ")}</p>
+                </div>
+                </div>
+        `;
+  } catch (error) {
+    console.error("Error", error);
+
+    infoContainer.innerHTML = `
+        <p>Erro: Pokemon not found, try again in a few minutes...</p>
+        `;
+  }
 }
 
-form.addEventListener('submit', getPokemon)
+form.addEventListener("submit", getPokemon);
